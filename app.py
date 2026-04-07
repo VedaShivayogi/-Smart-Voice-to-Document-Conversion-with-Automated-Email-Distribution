30253134abbd482cb0d5e60b18ca505a17043412
VEDA SHIVAYOGI RAMAGONDANAHALLI <vedaram2002@gmail.com>
2026-04-07 17:08:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app.secret_key = "secret123"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'vedaram2002@gmail.com'
-app.config['MAIL_PASSWORD'] = 'zrtlytzthpzpungy'   # Gmail App Password
-app.config['MAIL_DEFAULT_SENDER'] = 'vedaram2002@gmail.com'
+app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
+app.config['MAIL_PASSWORD'] = 'your_app_password'   # Gmail App Password
+app.config['MAIL_DEFAULT_SENDER'] = 'your_email@gmail.com'
 
 mail = Mail(app)
 
